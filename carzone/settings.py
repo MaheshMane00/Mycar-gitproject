037eba291dc7fab3e4dc4e64b2dd47f1182b0794
MaheshMane00 <mahesh841081mane@gmail.com>
2025-04-06 02:51:34 +0530

    Completed Teams Model

--- carzone/settings.py
+++ carzone/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,8 +78,11 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carzone_db',
+        'USER': 'postgres',
+        'PASSWORD': 'Mahesh',
+        'HOST': 'localhost',
     }
 }
 
@@ -116,9 +121,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS=[
+os.path.join(BASE_DIR, 'carzone/static'),
+  ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#media setting
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_URL='/media/' 
--- pages/urls.py
+++ pages/urls.py
@@ -3,4 +3,9 @@ from.import views
 
 urlpatterns = [
     path('home/',views.home,name='home'),
+    path('cars/',views.cars,name='cars'),
+    path('about',views.about,name='about'),
+    path('sevices',views.services,name='services'),
+    path('contact',views.contact,name='contact'),
+
 ]
